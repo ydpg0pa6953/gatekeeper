562d0d84238db9eb2760d9de460bc0ea04a4fbdf
ydpg0pa6953 <kansongliang79@126.com>
2017-11-25 17:37:30 +0800

    双通

--- module/ondata.js
+++ module/ondata.js
@@ -7,14 +7,13 @@ module.exports = {
     let server = store.server[sPort]
     if (!server || !server.connections) return
     let sc = server.connections
-    let s = null
-    for (s in sc) break;
-    if (!s || !sc[s]) return
+    // let s = null
+    // for (s in sc) break
+    // if (!s || !sc[s]) return
 
-
-    // for (let s in sc) {
-    //   sc[s] && sc[s].write(data)
-    // }
+    for (let s in sc) {
+      sc[s] && sc[s].write(data)
+    }
   },
   fromServer (sock, data) {
     let tPort = store.t_start + sock.localPort - store.s_start
@@ -22,13 +21,12 @@ module.exports = {
     if (!terminal) return
     if (!terminal || !terminal.connections) return
     let tc = terminal.connections
-    let t = null
-    for (t in tc) break;
-    if (!t || !tc[t]) return
+    // let t = null
+    // for (t in tc) break;
+    // if (!t || !tc[t]) return
 
-
-    // for (let t in tc) {
-    //   tc[t] && tc[t].write(data)
-    // }
+    for (let t in tc) {
+      tc[t] && tc[t].write(data)
+    }
   }
 }
